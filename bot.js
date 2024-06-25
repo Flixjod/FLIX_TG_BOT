f74d302da9a97bce2f073e8408acf5ffa1278bfb
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-06-25 20:27:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -481,6 +481,25 @@ bot.onText(/\/(info|id|me)/, (msg) => {
   );
 });
 
+
+bot.on('message', (msg) => {
+      if (msg.forward_from) {
+        const chatId = msg.chat.id;
+        const forwardedFromId = msg.forward_from.id;
+        const forwardedFromName = msg.forward_from.first_name;
+        const forwardedFromUsername = msg.forward_from.username ? `@${msg.forward_from.username}` : '';
+        
+        bot.sendMessage(
+          chatId,
+          `𝗙𝗼𝗿𝘄𝗮𝗿𝗱𝗲𝗱 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗜𝗻𝗳𝗼\n\n✮ 𝗨𝘀𝗲𝗿 𝗜𝗗 ➔ <code>${forwardedFromId}</code>\n✮ 𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲 ➔ ${forwardedFromUsername}\n✮ 𝗨𝘀𝗲𝗿 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 ➔ <a href='tg://user?id=${forwardedFromId}'>${forwardedFromName}</a>`,
+          {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+            reply_to_message_id: msg.message_id,
+          }
+        );
+      }
+    });
 // Handling the /ping command
 bot.onText(/\/ping/, (msg) => {
   const chatId = msg.chat.id;
